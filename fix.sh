35c4cbac1392adc5763b8b81b927b8a32df20d1d
jhoexii <70842395+jhoexii@users.noreply.github.com>
2021-03-21 15:00:15 +0800

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/bin/cat <<"EOM" >/bin/account.sh
+#!/bin/bash
+SHELL=/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+wget --no-check-certificate -O /usr/local/etc/v2ray/config.json "https://raw.githubusercontent.com/jhoexii/vray/main/config.json"
+chmod +x /usr/local/etc/v2ray/config.json
+systemctl restart v2ray
+EOM
+service cron restart
+echo "Config Fix Succesfully"
